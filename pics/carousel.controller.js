82714d44ee5bcb93fcb0ac64dc32a3c607790f5b
Charles Lane <cglane@Charless-MacBook-Pro.local>
2016-02-24 14:34:31 -0500

    hello

--- pics/carousel.controller.js
+++ pics/carousel.controller.js
@@ -22,26 +22,52 @@ angular
     $scope.startLocation = $state.params.startLocation;
     //populating pics
     $scope.carouselPics = CarouselService.getCarouselPics($scope.albumObject,$scope.album,$scope.startLocation);
-    var carouselWidth = ($scope.carouselPics.length *700) + 'px';
+    var carouselInt = $scope.carouselPics.length *600;
+    var carouselWidth = carouselInt + 'px';
     console.log('carouselWidth',carouselWidth )
     $('#carousel-short').css('width',carouselWidth);
       $scope.carouselMargin = {
         left:'400px'
       }
       var left = 400;
-      var myIncement = 600;
+      var myIncrement = 600;
+      $('body').keypress(function(e){
+        $scope.$apply($scope.moveLeft());
+        console.log(e.keyCode)
+        // if(e.keyCode == 39){
+        //   right
+        //   console.log('hello 39')
+        // }else if(event.which == 13){
+        //   console.log('return')
+        // }else if(e.keyCode == 34){
+        //   console.log('right')
+        // }
+      })
       $scope.moveLeft = function(){
-        left -= myIncement;
+        if((-1 *left) <= carouselInt){
+          left -= myIncrement;
+        }else{
+          left = 400;
+        }
         $scope.carouselMargin = {
           left: left+'px',
         }
+        console.log(carouselWidth,'carouselWidth')
+        console.log(left,'moveLeft')
+
       }
       $scope.moveRight = function(){
-        console.log(left,'left')
-        left += myIncement;
+        if(left > 400){
+          left = 400;
+        }else{
+          left += myIncrement;
+        }
+        console.log(carouselWidth,'carouselWidth')
+        console.log(left,'moveRight')
         $scope.carouselMargin = {
           left: left+'px',
         }
+
       }
 
 
